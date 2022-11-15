66ae7b65061092d0833957afdd98ec76ea8c4f3e
Christopher.Goodwin <christopher.goodwin@serko.com>
2022-11-15 13:50:39 +1300

    Add rounded corners

--- ui/Hero.tsx
+++ ui/Hero.tsx
@@ -7,9 +7,9 @@ export const Hero: React.FC = () => {
       <div className="absolute inset-0">
         <HeroBackground />
       </div>
-      <div className="hero min-h-screen bg-base-200">
+      <div className="hero min-h-screen bg-base-200 ">
         <div className="hero-content text-center">
-          <div className="max-w-xl">
+          <div className="max-w-xl backdrop-blur-sm bg-base-100/50 p-4 rounded-lg shadow-2xl">
             <h1 className="font-hero text-9xl font-bold">Ivory</h1>
             <p className="py-6">RPG dice rolling app built for the web</p>
             <CreateDiceRoom />
