fd404f8a2112d28da0f7194f5b718f950ef238a5
Christopher.Goodwin <christopher.goodwin@serko.com>
2022-11-22 09:09:33 +1300

    Enable RLS and create trigger for creating public users

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -26,6 +26,44 @@ create table public.dice_rolls (
 );
 comment on table public.dice_rolls is 'Individual dice_rolls sent by each user.';
 
+-- Secure the tables
+alter table public.users enable row level security;
+alter table public.channels enable row level security;
+alter table public.messages enable row level security;
+-- alter table public.user_roles enable row level security;
+-- alter table public.role_permissions enable row level security;
+-- create policy "Allow logged-in read access" on public.users for select using ( auth.role() = 'authenticated' );
+create policy "Allow individual insert access" on public.users for insert with check ( auth.uid() = id );
+create policy "Allow individual update access" on public.users for update using ( auth.uid() = id );
+-- create policy "Allow logged-in read access" on public.channels for select using ( auth.role() = 'authenticated' );
+create policy "Allow individual insert access" on public.channels for insert with check ( auth.uid() = created_by );
+-- create policy "Allow individual delete access" on public.channels for delete using ( auth.uid() = created_by );
+-- create policy "Allow authorized delete access" on public.channels for delete using ( authorize('channels.delete', auth.uid()) );
+create policy "Allow logged-in read access" on public.messages for select using ( auth.role() = 'authenticated' );
+create policy "Allow individual insert access" on public.messages for insert with check ( auth.uid() = user_id );
+-- create policy "Allow individual update access" on public.messages for update using ( auth.uid() = user_id );
+-- create policy "Allow individual delete access" on public.messages for delete using ( auth.uid() = user_id );
+-- create policy "Allow authorized delete access" on public.messages for delete using ( authorize('messages.delete', auth.uid()) );
+create policy "Allow individual read access" on public.user_roles for select using ( auth.uid() = user_id );
+
+
+-- inserts a row into public.users and assigns roles
+create function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.users (id, display_name)
+  values (new.id, new.raw_user_meta_data::json->>'display_name');
+
+  return new;
+end;
+$$ language plpgsql security definer;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
+
 -- Send "previous data" on change 
 alter table public.users replica identity full; 
 alter table public.channels replica identity full; 
